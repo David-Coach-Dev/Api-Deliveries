62028a95c9a2f5d39a531a91f22981f980b721f2
Dc Dev <69812733+cjcorpdev@users.noreply.github.com>
2022-08-05 01:25:09 -0500

    act

--- routers/router.php
+++ routers/router.php
@@ -10,6 +10,7 @@
    ********************************************/
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Credentials: true');
+    header('content-type: application/json; charset=utf-8');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
   /********************************
    ** No hay Petición en la api
