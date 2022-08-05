d6afe91b018c5b2fb755e980e56abe311178a794
Dc Dev <69812733+cjcorpdev@users.noreply.github.com>
2022-08-05 01:42:12 -0500

    act

--- routers/router.php
+++ routers/router.php
@@ -9,7 +9,7 @@
    *! Header.
    ********************************************/
     header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    header("Access-Control-Allow-Headers: Content-Type: application/json, Accept");
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     header('content-type: application/json; charset=utf-8');
   /********************************
