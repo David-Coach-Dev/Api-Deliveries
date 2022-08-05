3a8514e57a94469d47ed4f1f698918e0079ba09a
Dc Dev <69812733+cjcorpdev@users.noreply.github.com>
2022-08-05 01:35:23 -0500

    act

--- routers/router.php
+++ routers/router.php
@@ -9,9 +9,9 @@
    *! Header.
    ********************************************/
     header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Credentials: true');
-    header('content-type: application/json; charset=utf-8');
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('content-type: application/json; charset=utf-8');
   /********************************
    ** No hay Petición en la api
    ********************************/
