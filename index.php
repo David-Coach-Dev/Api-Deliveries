715c65c94348513e5cf9d77e647b3e5938780da5
Dc Dev <69812733+cjcorpdev@users.noreply.github.com>
2022-08-06 00:59:44 -0500

    act

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
    *******************************/
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, content-type, Accept");
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
     header('content-type: application/json; charset=utf-8');
   /*******************************
    ** Requerimientos
