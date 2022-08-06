936698205e82be818949152afaf4d829622b957f
Dc Dev <69812733+cjcorpdev@users.noreply.github.com>
2022-08-05 22:14:54 -0500

    act

--- controllers/post.controller.php
+++ controllers/post.controller.php
@@ -278,7 +278,6 @@
        *******************************/
           public function fncResponse($response, $method, $error){
             if(!empty($response)){
-              echo'<pre>';print_r(count($response));echo' - ';print_r($response);echo'</pre>';
               $json = array(
                 "status" => 201,
                 "method" => $method,
