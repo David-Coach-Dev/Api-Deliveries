90946cc0c04d9bd443fd60c7aac5b75a391c0aec
Dc Dev <69812733+cjcorpdev@users.noreply.github.com>
2022-08-06 01:12:45 -0500

    act

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
    ** Cors
    *******************************/
     header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Headers: Origin,Authorization, X-Requested-With, Content-Type, Accept");
     header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
-    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+    header('Access-Control-Allow-Credentials: true');
     header('content-type: application/json; charset=utf-8');
   /*******************************
    ** Requerimientos
