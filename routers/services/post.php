d182ba52ae4c5da91a2b16527b54594c0945b88f
Dc Dev <69812733+cjcorpdev@users.noreply.github.com>
2022-08-08 23:43:05 -0500

    act

--- routers/services/post.php
+++ routers/services/post.php
@@ -19,13 +19,13 @@
          ********************************************/
             if(isset($_POST)){
                 $_POST = file_get_contents("php://input");
-                echo'<pre> p: ';print_r($_POST);echo'</pre>';
+                echo'<pre> p1: ';print_r($_POST);echo'</pre>';
                 $_POST = explode("&", $_POST);
                 echo'<pre> p: ';print_r($_POST);echo'</pre>';
                 foreach(array_keys($_POST) as $key => $value){
                     array_push($columns, $value);
                 }
-                echo'<pre> p: ';print_r($columns);echo'</pre>';
+                echo'<pre> c: ';print_r($columns);echo'</pre>';
             }
         /********************************************
          *? Validar la tabla y columnas
