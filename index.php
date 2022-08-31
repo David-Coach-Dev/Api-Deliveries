d8b19d86d67cf170b671b33f9ce82b6feb58ed8d
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2022-08-31 12:26:50 -0500

    act

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     header("Access-Control-Allow-Headers: Origin, Authorization, X-Requested-With, XMLHttpRequest, Content-Type, Accept");
     header('Access-Control-Allow-Methods: GET, POST,OPTIONS, OPTIONS, PUT, DELETE');
     header('Access-Control-Allow-Credentials: true');
-    header('content-type: application/json; application/x-www-form-urlencoded; multipart/form-data; text/plain; charset=utf-8;');
+    header('content-type: application/json; application/x-www-form-urlencoded; multipart/form-data;boundary=something; text/plain; charset=utf-8;');
     header("Allow: GET, POST, OPTIONS, PUT,  DELETE");
     if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {header("HTTP/1.1 200 OK");}
   /*******************************
