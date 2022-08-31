f7af6d49e781e41f1dc43a20fe267abea05eb1a4
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2022-08-31 13:21:18 -0500

    a

--- routers/services/put.php
+++ routers/services/put.php
@@ -32,8 +32,10 @@
                     foreach($data as $key => $value){
                         array_push($data1, $value);
                     }
-                    echo "<pre> data1: ";print_r($data1->{'0'});echo"</pre>";
-                    $dato= explode("-",$data1->{'0'});
+                    echo "<pre> data1: ";print_r($data1);echo"</pre>";
+                    $dato= $data1->{'0'};
+                    echo "<pre> data1: ";print_r($dato);echo"</pre>";
+                    $dato= explode("-",$dato);
                     echo "<pre> dato: ";print_r($dato);echo"</pre>";
                     //echo "<pre> var_nbump : ";var_dump($dato);echo"</pre>";
                 /********************************************
